30bcc102f0aa321a8ae02329006ca053afe90b89
BRAST89 <100061375+BRAST89@users.noreply.github.com>
2023-02-21 11:35:09 -0500

    funcionalidades 1

---
+++ main.js
@@ -0,0 +1,125 @@
+// cambio cantidad de productos ingresados por el usuario
+let minusBtn = document.querySelector('.input__minus');
+let plusBtn = document.querySelector('.input__plus');
+let userInput = document.querySelector('.input__number');
+
+let userInputNumber = 0;
+
+plusBtn.addEventListener('click',()=>{
+    userInputNumber++;
+    userInput.value = userInputNumber
+    console.log(userInputNumber);
+})
+
+
+minusBtn.addEventListener('click',()=>{
+    userInputNumber--;
+    if(userInputNumber<=0){
+        userInputNumber = 0;
+    }
+    userInput.value = userInputNumber
+    console.log(userInputNumber);
+})
+
+
+// Agregar total de productos al carro
+
+const addToCartBtn = document.querySelector('.details__button');
+let cartNotification = document.querySelector('.header__cart--notification');
+
+let lastValue = parseInt(cartNotification.innerText);
+
+addToCartBtn.addEventListener('click', ()=>{
+    
+    lastValue = lastValue + userInputNumber;
+
+    cartNotification.innerText = lastValue;
+    cartNotification.style.display = 'block';
+    drawProductIntModal();
+    
+
+})
+
+// modal del ico-cart
+
+const cartIconBtn = document.querySelector('.header__cart');
+const cartModal = document.querySelector('.cart-modal');
+
+const productContainer = document.querySelector('.cart-modal__chekout-container');
+
+cartIconBtn.addEventListener('click',()=>{
+      cartModal.classList.toggle('show');
+
+      if(lastValue == 0){
+        productContainer.innerHTML = '<p class="cart-empty"> Your cart is empty </p>';       
+      }else{
+        drawProductIntModal();
+      }
+    }
+);
+
+// borrar el contenido del carrito
+function deleteProduct(){
+    const deleteProductBtn = document.querySelector('.cart-modal__delete');
+    deleteProductBtn.addEventListener('click',()=>{
+        productContainer.innerHTML = '<p class="cart-empty"> Your cart is empty </p>';
+        lastValue=0;
+        cartNotification.innerText = lastValue;
+    });
+}
+
+// image changes
+
+const imageContainer = document.querySelector('.gallery__image-container');
+const previousGalleryBtn = document.querySelector('.gallery__previus');
+const nextGalleryBtn = document.querySelector('.gallery__next');
+
+let imgIndex = 1;
+
+
+nextGalleryBtn.addEventListener('click', ()=>{
+    changeNextImage(imageContainer);
+});
+
+
+previousGalleryBtn.addEventListener('click', ()=>{
+    changeNextImage(imageContainer);
+});
+
+
+// functions
+
+function drawProductIntModal(){
+
+    productContainer.innerHTML =`<div class="cart-modal__details-container">
+        <img class="cart-modal__image" src="./images/image-product-1-thumbnail.jpg" alt="">
+        <div>
+           <p class="cart-modal__product">Autumn Limited Edition</p>
+           <p class="cart-modal__price">$125 x3 <span>$375.00</span></p>
+           </div>
+           <img class="cart-modal__delete" src="./images/icon-delete.svg" alt="delete">
+           </div>
+       <button class="cart-modal__checkout">Checkout</button>
+      </div>`
+    deleteProduct()
+    let priceModal = document.querySelector('.cart-modal__price');
+    priceModal.innerText = `$125 x${lastValue} <span>$${lastValue*125}.00</span>`
+}
+
+function changeNextImage(imgContainer){
+    if(imgIndex == 4){
+        imgIndex = 1;
+    }else{
+        imgIndex++;
+
+    }
+    imageContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
+}
+function changePreviousImage(imgContainer){
+    if(imgIndex == 1){
+        imgIndex = 4;
+    }else{
+        imgIndex--;
+    }
+    imageContainer.style.backgroundImage = `url('../images/image-product-${imgIndex}.jpg')`
+}
